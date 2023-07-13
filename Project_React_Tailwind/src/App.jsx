00eed5caf095db3c3356fc5968d94bfab8b9734c
Adrian Z <sylzyg555@interia.pl>
2023-07-13 19:12:16 +0200

    template Tailwind/React

---
+++ Project_React_Tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg font-bold underline bg-slate-800 decoration-pink-500 text-center" >
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
+ 
