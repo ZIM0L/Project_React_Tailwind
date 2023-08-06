ee4a2e4eee9ce9e43073760806d4e9df3b76987d
Adrian Z <sylzyg555@interia.pl>
2023-08-06 16:26:27 +0200

    correction

--- Project_React_Tailwind/src/main.jsx
+++ Project_React_Tailwind/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router >
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  </Router>
 )
